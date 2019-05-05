4c98a466c0d2a66103be8463f8d37f85ce9558c6
Xu Haoran <c170126@e.ntu.edu.sg>
2019-05-05 15:46:06 +0800

    hello word written in Vim

---
+++ Day01/hello.py
@@ -0,0 +1,15 @@
+"""
+yep, the typical hello world file 
+written in Vim
+To start my offical python learning journey
+
+Author: Xu Haoran
+Date:2019-05-05
+Version:0.1
+
+"""
+
+print ('hello,world!')
+print ('hello','python',sep=',',end='!\n')
+
+
